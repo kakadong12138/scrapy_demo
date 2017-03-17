d2ed48aeee150b5fcefb91d96c544ff273908d49
a123002@qq.com <a123002@qq.com>
2017-03-17 11:07:55 +0800

    增加保存失败request到redis的功能

--- dianping/dianping/spiders/dianping1.py
+++ dianping/dianping/spiders/dianping1.py
@@ -15,20 +15,25 @@ from MyBaseSpider import RedisSpider
 class RestaurantIdSpider(RedisSpider):
     name = "test1"
     allowed_domains = ["dianping.com"]
-    #handle_httpstatus_list = [404] 
+    handle_httpstatus_list = [301,302,403,404,500,12138]
+    retry_mode = False
     custom_settings = {
-        "USER_AGENT":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:45.0) Gecko/20100101 Firefox/45.0",
+        #"USER_AGENT":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:45.0) Gecko/20100101 Firefox/45.0",
         "REDIS_START_URLS_KEY":"dianping_test2"}
 
 
     def make_request_from_data(self, data):  
         city_id = data.get("city_id")
         url = "http://www.dianping.com/search/category/1/%s"%(city_id)
-        return Request(url,meta={"city_id":city_id})
+        return Request(url,meta={"task_json":data},callback=self.parse_1)
 
-    def parse(self,response):
+    def parse_1(self,response):
         meta = response.meta
-        print response
+        if response.status == 12138:
+            item = self.parse_error(response)
+            print item
+            yield item
+        
 
 
 
